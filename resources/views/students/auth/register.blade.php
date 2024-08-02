17b7bc3ff3f772b76ead2c58005662f9a279050a
ccrpytoDev <smartwebdev90@gmail.com>
2024-08-02 04:50:16 +0200

    modified user sign, signup

--- resources/views/students/auth/register.blade.php
+++ resources/views/students/auth/register.blade.php
@@ -1,85 +1,72 @@
 @extends('frontend.layouts.app')
-@section('title', 'Sign Up')
-@section('header-attr') class="nav-shadow" @endsection
-
+@section('custom-style')
+    <link rel="stylesheet" href="{{ asset('assets/css/login.css') }}">
+@endsection
 @section('content')
-<!-- SignUp Area Starts Here -->
-<section class="signup-area signin-area p-3">
-    <div class="container">
-        <div class="row align-items-center justify-content-md-center">
-            <div class="col-lg-5 order-2 order-lg-0">
-                <div class="signup-area-textwrapper">
-                    <h2 class="font-title--md mb-0">Sign Up</h2>
-                    <p class="mt-2 mb-lg-4 mb-3">Already have account? <a href="{{route('studentLogin')}}" class="text-black-50">Sign In</a></p>
-                    <form action="{{route('user.store','studentdashboard')}}" method="POST">
-                        @csrf
-                        <div class="form-element">
-                                <label for="name">Full Name</label>
-                                <input type="text" placeholder="Enter Your Name" id="name" value="{{old('name')}}" name="name" />
-                                @if($errors->has('name'))
-                                    <small class="d-block text-danger">{{$errors->first('name')}}</small>
-                                @endif
-                        </div>
-                        <div class="form-element">
-                                <label for="email">Email</label>
-                                <input type="email" placeholder="example@email.com" id="email" value="{{old('email')}}" name="email" />
-                                @if($errors->has('email'))
-                                    <small class="d-block text-danger">{{$errors->first('email')}}</small>
-                                @endif
-                        </div>
-                        <div class="form-element">
-                            <label for="password" class="w-100" style="text-align: left;">password</label>
-                            <div class="form-alert-input">
-                                <input type="password" placeholder="Type here..." id="password"  name="password"/>
-                                <div class="form-alert-icon" onclick="showPassword('password',this)">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                        stroke-linejoin="round" class="feather feather-eye">
-                                        <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                                        <circle cx="12" cy="12" r="3"></circle>
-                                    </svg>
-                                </div>
-                                @if($errors->has('password'))
-                                    <small class="d-block text-danger">{{$errors->first('password')}}</small>
-                                @endif
-                            </div>
-                        </div>
-                        <div class="form-element">
-                            <label for="password_confirmation" class="w-100" style="text-align: left;">Confirm
-                                password</label>
-                            <div class="form-alert-input">
-                                <input type="password" placeholder="Type here..." name="password_confirmation" id="password_confirmation" />
-                                <div class="form-alert-icon" onclick="showPassword('password_confirmation',this)">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                        stroke-linejoin="round" class="feather feather-eye">
-                                        <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                                        <circle cx="12" cy="12" r="3"></circle>
-                                    </svg>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-element d-flex align-items-center terms">
-                            <input class="checkbox-primary me-1" type="checkbox" id="agree" />
-                            <label for="agree" class="text-secondary mb-0">Accept the <a href="#"
-                                    style="text-decoration: underline;">Terms</a> and <a href="#"
-                                    style="text-decoration: underline;">Privacy Policy</a></label>
-                        </div>
-                        <div class="form-element">
-                            <button type="submit" class="button button-lg button--primary w-100">Sign UP</button>
-                        </div>
-                    </form>
+    <section class="login">
+        <div class="container">
+            <div class="login__wrapper">
+                <div class="login__bg">
+                    <img src="{{asset('assets/img/content/login/img.jpg')}}" class="login__bg__img" alt="">
+                    <img class="login__bg__angle" src="{{asset('assets/img/content/login/angle.png')}}" alt="">
                 </div>
-            </div>
-            <div class="col-lg-7 order-1 order-lg-0">
-                <div class="signup-area-image">
-                    <img src="{{asset('frontend/dist/images/signup/Illustration.png')}}" alt="Illustration Image"
-                        class="img-fluid" /> 
-                </div>
+
+                <h1 class="h1">הירשם</h1>
+
+                <form onsubmit = "return(signUp());" novalidate  action="{{route('user.store', 'user.login')}}" class="login__form" method="POST">
+                    @csrf
+                    <div class="login__form__login">
+                        <input type="text" required 
+                            class="login__form__input login__form__input_login @error('name') login__form__error @enderror" 
+                            name="name" value="{{old('name')}}" placeholder="שֵׁם">
+                        <span class="login__form__err">שֵׁם</span>
+                    </div>
+                    @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+
+                    <div class="login__form__login">
+                        <input type="text" required 
+                            class="login__form__input login__form__input_login @error('email') login__form__error @enderror" 
+                            name="email" value="{{old('email')}}" placeholder="אימייל">
+                        <span class="login__form__err">שְׁגִיאָה</span>
+                    </div>
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+
+                    <div class="login__form__password login__form__password1">
+                        <button type="button" class="login__form__password__show"></button>
+                        <input type="password" required class="login__form__input login__form__input_password @error('password')login__form__error @enderror" name="password" placeholder="סיסמה">
+                        <span class="login__form__err">שְׁגִיאָה</span>
+                    </div>
+                    @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+
+                    <div class="login__form__password login__form__password2">
+                        <button type="button" class="login__form__password__show"></button>
+                        <input type="password" required class="login__form__input login__form__input_password" name="password_confirmation" placeholder="סיסמה">
+                        <span class="login__form__err">שְׁגִיאָה</span>
+                    </div>
+
+                    <button type="submit" class="login__form__submit btn">הירשם</button>
+
+                    <div class="login__form__wrapper">
+                        <a class="login__form__link" href="{{route('user.login')}}">התחברות</a>
+                    </div>
+                </form>
             </div>
         </div>
-    </div>
-</section>
-<!-- SignUp Area Ends Here -->
+    </section>
+@endsection
 
+@section('custom-script')
+    <script src="{{asset('assets/js/login.js')}}"></script>
 @endsection
